21405bf365409e4783fb3b18e551b923f15a57ac
Sandro Stricker <sandro.stricker@roqapps.com>
2025-11-10 20:13:05 +0100

    Bump to version 11.0.0

--- Package.swift
+++ Package.swift
@@ -36,14 +36,14 @@ let package = Package(
     .binaryTarget(
       name: "InMobiAdapter",
       url:
-        "https://dl.google.com/googleadmobadssdk/mediation/ios/inmobi/InMobiAdapter-10.8.8.0.zip",
-      checksum: "96525e393eb97f3b392943f17cd69a8641c486bc3da71aaa675678988f99d797"
+        "https://dl.google.com/googleadmobadssdk/mediation/ios/inmobi/InMobiAdapter-11.0.0.0.zip",
+      checksum: "ea85bb36a3568037a5cb83fbd44033fbc3185a8b85e7288c7375a45c690af2ea"
     ),
     .binaryTarget(
       name: "InMobi-iOS-SDK",
       url:
-        "https://dl.inmobi.com/inmobi-sdk/IM/InMobi-iOS-SDK-10.8.8.zip",
-      checksum: "638166a04eb3940b2caa968fc4f5cbbc42336416db430cab45db95070d78817b"
+        "https://dl.inmobi.com/inmobi-sdk/IM/InMobi-iOS-SDK-11.0.0.zip",
+      checksum: "feca177cc17ee472c736268873dc66e157484ffe6add981375b802f4dc9bc1c0"
     ),
   ]
 )
